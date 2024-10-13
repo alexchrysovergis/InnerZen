7a6e29f00811810bb0dbea13484c8a9fbb7310db
alexchrysovergis <alexandroschrysovergis@gmail.com>
2024-10-13 14:51:20 -0600

    Tailwind Init

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,26 +1,26 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
 const cleanCSS = require('gulp-clean-css');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 const concat = require('gulp-concat');
 
 const paths = {
-    scss: './scss/*.scss',
-    cssDest: './../htdocs/dist/css/'
+  scss: './scss/*.scss',
+  cssDest: '../htdocs/dist/css/'
 };
 
 function css() {
-    return src(paths.scss) // the src
-        .pipe(sass())
-        .pipe(concat('styles.min.css'))  // concat the scss into a single file
-        .pipe(cleanCSS())  // minify it
-        .pipe(dest(paths.cssDest)); //the compiled css destination
+  return src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      tailwindcss('./tailwind.config.js'),
+      autoprefixer()
+    ]))
+    .pipe(concat('styles.min.css'))
+    .pipe(cleanCSS())
+    .pipe(dest(paths.cssDest));
 }
 
-function watchFiles() {
-    watch(paths.scss, css); // Watch sass files and run the css task on save
-}
-
-exports.css = css;
-exports.watch = watchFiles;
-
-exports.default = series(css, watchFiles);
+exports.default = series(css);
